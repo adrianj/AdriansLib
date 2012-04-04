4ca6d11451f5e672ae37e5b42fec5eef089a9534
Adrian Jongenelen <adrianj@dta.mil.nz>
2012-04-04 16:50:23 +1200

    Implemented CSVWriter

--- AdriansLibClient/TestCsvWriter.cs
+++ AdriansLibClient/TestCsvWriter.cs
@@ -27,10 +27,37 @@ namespace AdriansLibClient
 			CurveList cl = new CurveList();
 			cl.Add(new LineItem(labels[0], x1, y1, Color.Black, SymbolType.Circle));
 			cl.Add(new LineItem(labels[1], x2, y2, Color.Black, SymbolType.Circle));
-
-			CsvWriter.ExportToCsv(cl, folder + labels[0]);
+			string filename = folder + labels[0]+".csv";
+			Console.WriteLine("Writing for file: " + filename);
+			CsvWriter.ExportToCsv(cl, filename);
 			
 		}
 
+
+		[Test]
+		public void TestComplexData()
+		{
+
+			string[] labels = { "complex1", "complex2","complex3" };
+			double[] x1 = { 4e12, 5e13, 6e14 };
+			double[] y1 = { 12e-10, 15e-10, 18e-10 };
+			double[] x2 = { -2, -3, -4, -5, -6, -7, -8, -9, -10, -11 };
+			double[] y2 = { 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, 100000, 1000000 };
+			double[] z2 = { 1.23453456345345345, 2.6450985347635, 3.45237895638925, 4.42378962387534, 5.4532498734563, 
+							  6.429874343, 7.5349857345, 8.5347637634, 9.3764684593453, 10.53948756876345 };
+			double[] x3 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+			double[] y3 = new double[x3.Length];
+			CurveList cl = new CurveList();
+			cl.Add(new LineItem(labels[0], x1, y1, Color.Black, SymbolType.Circle));
+			PointPairList ppl = new PointPairList();
+			for(int i = 0; i < x2.Length; i++)
+				ppl.Add(x2[i],y2[i],z2[i]);
+			cl.Add(new LineItem(labels[1], ppl, Color.Black, SymbolType.Circle));
+			cl.Add(new LineItem(labels[2], x3, y3, Color.Black, SymbolType.Circle));
+			string filename = folder + labels[0] + ".csv";
+			Console.WriteLine("Writing for file: " + filename);
+			CsvWriter.ExportToCsv(cl, filename);
+
+		}
 	}
 }
--- ComplexMath/CsvWriter.cs
+++ ComplexMath/CsvWriter.cs
@@ -11,8 +11,12 @@ namespace ComplexMath
 {
 	public class CsvWriter
 	{
+		static bool firstElementInLine = true;
+		static List<bool[]> validDimensions;
+
 		public static void ExportToCsv(CurveList curves, string filename)
 		{
+			validDimensions = CalculateValidDimensions(curves);
 			using (StreamWriter writer = new StreamWriter(filename, false, ASCIIEncoding.ASCII))
 			{
 				WriteHeaders(curves, writer);
@@ -22,12 +26,98 @@ namespace ComplexMath
 
 		static void WriteHeaders(CurveList curves, StreamWriter writer)
 		{
+			firstElementInLine = true;
+			for(int i = 0; i < curves.Count; i++)
+			{
+				WriteHeaders(curves[i], validDimensions[i], writer);
+			}
+			writer.WriteLine();
+		}
 
+		static void WriteHeaders(CurveItem curve, bool [] validDims, StreamWriter writer)
+		{
+			if (validDims[0]) WriteElement(curve.Label.Text + "_X", writer);
+			if (validDims[1]) WriteElement(curve.Label.Text + "_Y", writer);
+			if (validDims[2]) WriteElement(curve.Label.Text + "_Z", writer);
 		}
 
 		static void WriteData(CurveList curves, StreamWriter writer)
 		{
+			int maxPoints = CalculateMaxPoints(curves);
+			Console.WriteLine(maxPoints);
+			for (int i = 0; i < maxPoints; i++)
+			{
+				Console.WriteLine("line: " + i);
+				firstElementInLine = true;
+				WriteDataRow(curves, i, writer);
+				writer.WriteLine();
+			}
+		}
 
+		static void WriteDataRow(CurveList curves, int row, StreamWriter writer)
+		{
+			for(int i = 0; i < curves.Count; i++)
+				WriteDataRow(curves[i], validDimensions[i], row, writer);
+		}
+
+		static void WriteDataRow(CurveItem curve, bool[] validDims, int row, StreamWriter writer)
+		{
+			if (row < curve.NPts)
+			{
+				if (validDims[0]) WriteDouble(curve.Points[row].X, writer);
+				if (validDims[1]) WriteDouble(curve.Points[row].Y, writer);
+				if (validDims[2]) WriteDouble(curve.Points[row].Z, writer);
+			}
+			else
+			{
+				if (validDims[0]) WriteElement("", writer);
+				if (validDims[1]) WriteElement("", writer);
+				if (validDims[2]) WriteElement("", writer);
+			}
+		}
+
+		static void WriteDouble(double d, StreamWriter writer)
+		{
+			string s = d.ToString();
+			if (double.IsNaN(d))
+				s = "";
+			WriteElement(s, writer);
+		}
+
+		static void WriteElement(string s, StreamWriter writer)
+		{
+			if (!firstElementInLine) writer.Write(','); 
+			firstElementInLine = false;
+			writer.Write(s);
+		}
+
+		static List<bool[]> CalculateValidDimensions(CurveList curves)
+		{
+			List<bool[]> ret = new List<bool[]>();
+			foreach (CurveItem curve in curves)
+				ret.Add(DimensionContainsNonZeroData(curve));
+			return ret;
+		}
+
+		static bool[] DimensionContainsNonZeroData(CurveItem curve)
+		{
+			bool[] ret = new bool[3];
+			for(int i = 0; i < curve.NPts; i++)
+			{
+				PointPair pp = curve.Points[i];
+				if (pp.X != 0) ret[0] = true;
+				if (pp.Y != 0) ret[1] = true;
+				if (pp.Z != 0) ret[2] = true;
+			}
+			return ret;
+		}
+
+		static int CalculateMaxPoints(CurveList curves)
+		{
+			int max = 0;
+			foreach (CurveItem curve in curves)
+				max = Math.Max(max, curve.NPts);
+			return max;
 		}
 	}
 }
